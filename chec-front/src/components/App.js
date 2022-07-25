de35d449b425b25a997f942d22d7d4904a1fbfa5
“AraceliCatalano” <aracelicatalano@gmail.com>
2022-07-25 18:04:14 -0300

    add react project and home - Css not connected, pending fix

---
+++ chec-front/src/components/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Header'
+
+function App() {
+  return (
+    <div className="App">
+     <p>*****Chec App ****</p>
+     <Header/>
+    </div>
+  );
+}
+
+export default App;
