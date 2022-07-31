b1cf32575e7cf7a8f6917045830bc5b7bbbefa0f
“AraceliCatalano” <aracelicatalano@gmail.com>
2022-07-31 20:38:25 -0300

    Dashboard verificado ok

--- chec-front/src/components/GnHeader.js
+++ chec-front/src/components/GnHeader.js
@@ -13,41 +13,15 @@ function GnHeader() {
                 <nav className="main-navbar">
                     <Link to="/"><img src={logo} className="logo" alt="logo"/></Link>
                     <ul className="left-navbar">                
-                        <li><Link to="/">Home </Link></li>
+                        
                         <li><Link to="/dashboard">Dashboard </Link></li>
-                        <li><Link to="/quienesSomos">Quienes somos </Link></li>
-                        <li><Link to="/recetas/nuestrasRecetas">Nuestras Recetas </Link></li>
+                        
+                        {/* <li><Link to="/quienesSomos">Quienes somos </Link></li> */}
+                        {/* <li><Link to="/recetas/nuestrasRecetas">Nuestras Recetas </Link></li> */}
                     </ul>
-                    <ul className="rigth-navbar">
-                        <li>
-                            <div className="pd-pc-links">
-                                <Link to="/product/productPage" className="tienda-botton">Tienda</Link>
-                                <Link to="/sales/productCart"><i className="fas fa-shopping-cart"></i></Link> 
-                                                
-                            </div>
-                        </li>
-                        <li>
-                            <ul className="dropdown-menu-container">
-                                {/* <% if (locals && locals.isLogged) { %> */}
-                                    <li>
-                                        {/* <img src="/images/avatars/<%= locals.userLogged.avatar %>" alt="avatar"/>  */}
-                                        {/* <a><%= locals.userLogged.name %></a> */}
-                                        
-                                        <ul className="dropdown-menu">
-                                            <li><a href="/users/profile">Mi perfil</a></li>
-                                            <li><a href="/users/logout">Salir</a></li>
-                                        </ul>
-                                    </li>
-                                {/* <% } else if(locals && !locals.isLogged){ %> */}
-                                    <li><a href="/users/login">Ingresá <i className="fas fa-sign-in-alt"></i></a></li>
-                                {/* <% } else {%>
-                                    <li><span>No hay locals!</span></li>
-                                <% } %> */}
-                            </ul>
-                        </li>
-                    </ul>
+                    
                 </nav> 
-                <div className="secondary-navbar">
+                {/* <div className="secondary-navbar">
                     <div className="hamburguer-button">
                         <i className="fas fa-bars"></i> 
                     </div>
@@ -60,7 +34,7 @@ function GnHeader() {
                         <Link to="#" className="redes2"><i className="fab fa-facebook"></i></Link>
                         <Link to="https://instagram.com/chec.cerveceria?utm_medium=copy_link" target="_blank" className="redes2"><i className="fab fa-instagram"></i></Link>
                     </div>           
-                </div>
+                </div> */}
         </header>
         </div>
 
